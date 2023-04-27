72394510d1eb2eb04a8c51f82ef3a354b6048525
Ali <128646611+Skaybun@users.noreply.github.com>
2023-04-28 00:16:56 +0300

    Add files via upload

---
+++ H4CKER News/H4CKER_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostData.swift
+//  H4XOR News
+//
+//  Created by Ali KINU on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct H4CKER_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4CKER News/View/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  PostData.swift
+//  H4XOR News
+//
+//  Created by Ali KINU on 20.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
